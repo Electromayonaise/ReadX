43dbdffff592539998039bae5f1d829d6ae17057
Martín Gómez <123976183+Electromayonaise@users.noreply.github.com>
2023-05-03 10:01:05 -0500

    First version of the first two functional requirements

---
+++ src/model/BasicUser.java
@@ -0,0 +1,10 @@
+package model;
+import java.util.GregorianCalendar;
+
+public class BasicUser extends Users {
+
+    public BasicUser(String name, String id, GregorianCalendar registrationDate) {
+        super(name, id, registrationDate, 1);
+    }
+    
+}
---
+++ src/model/PremiumUser.java
@@ -0,0 +1,31 @@
+package model;
+import java.util.GregorianCalendar;
+
+public class PremiumUser extends Users {
+    
+    private String cardNumber;
+    private String securityCode;
+
+    public PremiumUser(String name, String id, GregorianCalendar registrationDate, String cardNumber, String securityCode) {
+        super(name, id, registrationDate,2);
+        this.cardNumber = cardNumber;
+        this.securityCode = securityCode;
+        
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public String getSecurityCode() {
+        return securityCode;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public void setSecurityCode(String securityCode) {
+        this.securityCode = securityCode;
+    }
+}
